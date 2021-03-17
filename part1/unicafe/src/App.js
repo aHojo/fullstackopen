7d7b82b7a48e0df10e79524b2c91a3e73d501b49
ahojo <ahojodev@gmail.com>
2021-03-17 11:36:18 -0400

    Started part1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function App() {
+  
+  // save clicks of each button
+  const [good, setGood] = useState(0);  
+  const [neutral, setNeutral] = useState(0);  
+  const [bad, setBad] = useState(0);  
+
+  return (
+    <React.Fragment>
+      code here
+    </React.Fragment>
+  );
+}
+
+export default App;
