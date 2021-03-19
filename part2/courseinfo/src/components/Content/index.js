6fa177a9f3ef310f8aa7fbabac5e66e09a388c7d
ahojo <ahojo@upenn.edu>
2021-03-19 11:37:03 -0400

    added total

--- part2/courseinfo/src/components/Content/index.js
+++ part2/courseinfo/src/components/Content/index.js
@@ -1,11 +1,18 @@
 import Part from '../Part';
 
+const Total = ({ course }) => {
+    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+    return (
+        <h3>Total of {sum} exercises</h3>
+    )
+}
 const Content = ({ course }) => {
     return (
         <div>
             {course.parts.map(part => {
                 return <Part key={part.id} part={part} />
             })}
+            <Total course={course} />
             
         </div>
     )
