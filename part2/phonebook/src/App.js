b0f269a1917364fe1b658ac000a8c904f9635b79
ahojo <ahojodev@gmail.com>
2021-03-23 10:10:52 -0400

    Added ability to post to the server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -16,12 +16,7 @@ function utilCheckIfNameExists(newName, persons) {
 
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNumber ] = useState('')
   const [filter, setFilter ] = useState('');
@@ -38,11 +33,18 @@ const App = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!utilCheckIfNameExists(newName, persons)){
-      setPersons(
-        [...persons, {name: newName, number: newNumber}]
-      );
-      setNewName('');
-      setNumber('');
+      
+      axios.post('http://localhost:3001/persons', {name: newName, number: newNumber})
+      .then(response => {
+        console.log("Response Data", response.data)
+        setPersons(
+          [...persons, response.data]
+        );
+        setNewName('');
+        setNumber('');
+      })
+      
+      
     } else {
       alert(`${newName}  already exists`)
     }
